2fd6c611c2eaabf99032e9621bfa27c6014976e8
Anany Gangver <gangver.ananya2001@gmail.com>
2023-06-15 23:09:33 +0530

    isAnagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,28 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length !== str2.length){
+      return false;
+  }
 
+  var frequency = new Array(26).fill(0);
+  var str11 = str1.toLowerCase();
+  var str22 = str2.toLowerCase();
+
+  for(let i = 0; i < str1.length; i++){
+      frequency[str11.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+      frequency[str22.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+  }
+
+  for(let i = 0 ; i < 26; i++){
+      if(frequency[i] !== 0)
+      return false;
+  }
+  return true;
 }
+
+console.log(isAnagram("raae","care"));
+
+module.exports = isAnagram;
 
 module.exports = isAnagram;
